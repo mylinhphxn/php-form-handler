1117cd1740c7c4c20edf70fb92ccdfd988925e76
mylinhphxn <103076867+mylinhphxn@users.noreply.github.com>
2022-05-26 16:43:07 +0000

    adds world's smallest form handler

--- index.php
+++ index.php
@@ -1,16 +1,22 @@
 <html>
   <head>
-    <title>PHP Test Page</title>
+    <title>World's Smallest Form Handler</title>
   </head>
   <body>
-    <h1>PHP Test Page - Can You See This?</h1>
-    <?php
-    echo '<p>This is PHP!</p>';
-    ?>
+    <h1>World's Smallest Form Handler</h1>
+  <?php
+  if(isset($_POST["FirstName"])){//show data 
+    echo $_POST["FirstName"];
+  }else{//show form
+    echo '
+          <form method="post" action="">
+          <p>First Name: <input type="text" name="FirstName"/></p>
+          <p><input type="submit" /></p>
+          </form>
+        ';
+  }
 
-     <?php
-    echo '<p>This is a change from replit!</p>';
-    ?>
-  
+  ?>
+
   </body>
 </html>
